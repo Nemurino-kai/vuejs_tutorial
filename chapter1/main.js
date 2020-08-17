97a53927610a27900cd6819e74a5df1b1f2886bd
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-17 17:18:24 +0900

    イベントの利用

--- chapter1/main.js
+++ chapter1/main.js
@@ -1,5 +1,10 @@
 var app = new Vue({
     el: '#app',
+    methods:{
+        handleClick: function(event){
+            alert(event.target) // [object HTMLButtonElement]
+        }
+    },
     data: {
         message :'Hello Vue.js!',
         list:['にんじん','サンダル','ヨット','ごましお']
