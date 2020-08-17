41ad2030456d4e5c527aa72064289837c2d9a69a
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-18 01:48:30 +0900

    フォーム入力の取得

--- chapter3/main.js
+++ chapter3/main.js
@@ -1,11 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
+        message: "kon~",
         show: true,
     },
     methods: {
         handleClick: function(event) {
             alert("クリックしたぜ")
+        },
+
+        handleInput: function(event) {
+            alert("変更したぜ")
         }
+
     }
 })
