ce227faff7d24714f27525117ce13468b702649a
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-17 17:24:04 +0900

    条件分岐

--- chapter1/main.js
+++ chapter1/main.js
@@ -1,13 +1,14 @@
 var app = new Vue({
     el: '#app',
-    methods:{
-        handleClick: function(event){
+    methods: {
+        handleClick: function (event) {
             alert(event.target) // [object HTMLButtonElement]
         }
     },
     data: {
-        message :'Hello Vue.js!',
-        count :5,
-        list :['にんじん','サンダル','ヨット','ごましお']
+        message: 'Hello Vue.js!',
+        show: true,
+        count: 5,
+        list: ['にんじん', 'サンダル', 'ヨット', 'ごましお']
     }
-  })
+})
