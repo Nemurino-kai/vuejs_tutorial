4df919b1d31f1e2f0f70b25a789d80a2993b9da2
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-17 18:17:09 +0900

    クラスとスタイルのデータバインディング

--- chapter2/main.js
+++ chapter2/main.js
@@ -1,12 +1,20 @@
 var app = new Vue({
     el: '#app',
     data: {
+        isChild: true,
+        isActive: true,
+        textColor: 'red',
+        bgColor: 'lightgray',
         count: 0,
         message: {
             value: 'Hello Vue.js!'
         },
         num: 1,
-        list: ['にんじん', 'サンダル', 'ヨット', 'ごましお']
+        list: ['にんじん', 'サンダル', 'ヨット', 'ごましお'],
+        styleObject: {
+            color: 'red',
+            backgroundColor: 'lightgray'
+        }
     },
     methods: {
         increment: function(){
