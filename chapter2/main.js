a486150aac1b380c66f16e00137c00624935bf90
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-17 18:08:18 +0900

    クリックでカウンターを増やそう

--- chapter2/main.js
+++ chapter2/main.js
@@ -1,10 +1,16 @@
 var app = new Vue({
     el: '#app',
     data: {
+        count: 0,
         message: {
             value: 'Hello Vue.js!'
         },
         num: 1,
         list: ['にんじん', 'サンダル', 'ヨット', 'ごましお']
+    },
+    methods: {
+        increment: function(){
+            this.count += 1
+        }
     }
 })
