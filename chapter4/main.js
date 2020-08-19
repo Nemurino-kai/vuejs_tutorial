8c7d28e5591389e150bfce78e425f8168dafc46c
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-19 20:51:09 +0900

    リストの絞り込みに利用しよう

--- chapter4/main.js
+++ chapter4/main.js
@@ -1,7 +1,16 @@
 var app = new Vue({
     el: '#app',
     data: {
-        width: 799
+        width: 799,
+        budget: 300,
+        limit: 2,
+        list: [
+            { id: 1, name: 'にんじん', price: 100 },
+            { id: 2, name: 'サンダル', price: 200 },
+            { id: 3, name: 'ヨット', price: 100000 },
+            { id: 4, name: 'ごましお', price: 1 },
+            { id: 5, name: 'ロケット', price: 3000000000 }
+        ]
     },
     computed: {
         halfWidth: {
@@ -12,7 +21,15 @@ var app = new Vue({
                 this.width = val * 2
             }
         },
-        computedData: function() { return Math.random() }
+        computedData: function() { return Math.random() },
+        matched: function() {
+            return this.list.filter(function(el) {
+                return el.price <= this.budget
+            }, this)
+        },
+        limited: function() {
+            return this.matched.slice(0, this.limit)
+        }
     },
     methods: {
         methodsData: function() { return Math.random() }
