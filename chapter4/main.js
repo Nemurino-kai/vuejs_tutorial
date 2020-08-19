35ee7798cc47f70815614fb187c144a166fbf652
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-19 20:32:04 +0900

    ゲッターとセッター

--- chapter4/main.js
+++ chapter4/main.js
@@ -1,21 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        width: 800,
-        height: 600
+        width: 799
     },
     computed: {
-        halfWidth: function() {
-            return this.width / 2
-        },
-        halfHeight: function() {
-            return this.height / 2
-        },
-        // 「width x height」の中心座標をオブジェクトで返す
-        halfPoint: function() {
-            return {
-                x: this.halfWidth,
-                y: this.halfHeight
+        halfWidth: {
+            get: function() {
+                return this.width / 2
+            },
+            set: function(val) {
+                this.width = val * 2
             }
         }
     }
