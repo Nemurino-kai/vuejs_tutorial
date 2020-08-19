9b56e778d28fe26cedd7b91e23e8a850079a7f30
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-20 08:17:21 +0900

    フォームを監視してAPIからデータを取得する

--- chapter4/main.js
+++ chapter4/main.js
@@ -1,13 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
+        list: [],
+        current: '',
+        topics: [
+            { value: 'vue', name: 'Vue.js' },
+            { value: 'jQuery', name: 'jQuery' }
+        ],
         message: "最初の一回だけ、変更を監視しています",
         edited: false,
         width: 799,
         order: false,
         budget: 300,
         limit: 2,
-        list: [
+        items: [
             { id: 1, name: 'にんじん', price: 100 },
             { id: 2, name: 'サンダル', price: 200 },
             { id: 3, name: 'ヨット', price: 100000 },
@@ -19,7 +25,6 @@ var app = new Vue({
 
         var unwatch = this.$watch('width', function(newVal, oldVal) {
             this.message = oldVal + "から" + newVal + "に変わりましたね～"
-                // listが編集されたことを記録する
             this.edited = true
                 // 監視を解除
             unwatch()
@@ -37,7 +42,7 @@ var app = new Vue({
         },
         computedData: function() { return Math.random() },
         matched: function() {
-            return this.list.filter(function(el) {
+            return this.items.filter(function(el) {
                 return el.price <= this.budget
             }, this)
         },
@@ -51,4 +56,16 @@ var app = new Vue({
     methods: {
         methodsData: function() { return Math.random() }
     },
+    watch: {
+        current: function(val) {
+            // GitHubのAPIからトピックのリポジトリを検索
+            axios.get('https://api.github.com/search/repositories', {
+                params: {
+                    q: 'topic:' + val
+                }
+            }).then(function(response) {
+                this.list = response.data.items
+            }.bind(this))
+        }
+    }
 })
