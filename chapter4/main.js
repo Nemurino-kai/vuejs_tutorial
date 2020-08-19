2501a61af3e10fddaef28ec66f8f91316b13cd98
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-20 08:29:45 +0900

    複数のフィルタをつなげて使用する

--- chapter4/main.js
+++ chapter4/main.js
@@ -71,6 +71,14 @@ var app = new Vue({
     filters: {
         localeNum: function(val) {
             return val.toLocaleString()
+        },
+        // 小数点以下を第2位に丸めるフィルタ
+        round: function(val) {
+            return Math.round(val * 100) / 100
+        },
+        // 度からラジアンに変換するフィルタ
+        radian: function(val) {
+            return val * Math.PI / 180
         }
     }
 })
