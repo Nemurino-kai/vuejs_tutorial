7e88404cb5db6a5f6c1e0dc050d44ca6fa60bdae
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-23 16:03:48 +0900

    移動トランジション

--- chapter6/main.js
+++ chapter6/main.js
@@ -2,7 +2,24 @@ new Vue({
     el: '#app',
     data: {
         show: true,
-        count: 0
-
+        count: 0,
+        order: false,
+        list: [
+            { id: 1, name: 'いちご', price: 100 },
+            { id: 2, name: 'にんじん', price: 200 },
+            { id: 3, name: 'サンダル', price: 300 },
+            { id: 4, name: 'ヨット', price: 300 },
+            { id: 5, name: 'ごましお', price: 300 },
+            { id: 6, name: 'ロケット', price: 300 },
+            { id: 7, name: '七面鳥', price: 300 },
+            { id: 8, name: 'はち', price: 300 },
+        ],
+    },
+    computed: {
+        // orderの値でリストの順番を反転する算出プロパティ
+        sortedList: function() {
+            // LodashのorderByメソッドを使用
+            return _.orderBy(this.list, 'id', this.order ? 'desc' : 'asc')
+        }
     }
 })
