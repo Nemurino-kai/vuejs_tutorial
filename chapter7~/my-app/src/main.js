61331c5011d8467bb8959488e565a1b7eff2b022
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-08-27 23:30:18 +0900

    ゲッター（getter）

--- chapter7~/my-app/src/main.js
+++ chapter7~/my-app/src/main.js
@@ -18,7 +18,5 @@ new Vue({
 })
 
 console.log(store.state.count) // -> 0
-// incrementをコミットする
-store.commit('increment')
 // もう一度アクセスしてみるとカウントが増えている
-console.log(store.state.count) // -> 1
+console.log(store.getters.max) // -> 1
